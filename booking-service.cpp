e7028b26de31c848174d0d2924a7057b602bc663
12proya21 <135091941+12proya21@users.noreply.github.com>
2023-07-20 11:19:45 +0500

    Update booking-service.cpp

--- booking-service.cpp
+++ booking-service.cpp
@@ -1,55 +1,74 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
-class RoomBookingService {
+class Product {
 public:
-    RoomBookingService() {}
+    Product(const std::string& name, double price)
+        : name_(name), price_(price) {}
 
-    void addRoom(const Room& room)
-    {
-        rooms_.push_back(room);
+    const std::string& getName() const {
+        return name_;
     }
 
-    bool bookRoom(int roomId, const std::string& user)
-    {
-        for (Room& room : rooms_) {
-            if (room.id == roomId && !room.isBooked) {
-                room.isBooked = true;
-                bookings_.push_back({ static_cast<int>(bookings_.size()), roomId, user });
-                return true;
-            }
-        }
-        return false;
+    double getPrice() const {
+        return price_;
     }
-
-    // Add other functions to list rooms, available rooms, bookings, etc.
 
 private:
-    std::vector<Room> rooms_;
-    std::vector<Booking> bookings_;
+    std::string name_;
+    double price_;
 };
 
-int main()
-{
-    RoomBookingService service;
-
-    // Add some sample rooms to the service
-    service.addRoom({ 1, "Room A", 10, false });
-    service.addRoom({ 2, "Room B", 20, false });
-    service.addRoom({ 3, "Room C", 15, false });
-
-    // Example: Booking Room 1 for "John Doe"
-    if (service.bookRoom(1, "John Doe")) {
-        std::cout << "Room booked successfully!" << std::endl;
-    } else {
-        std::cout << "Room is not available for booking." << std::endl;
+class ProductSearchService {
+public:
+    void addProduct(const Product& product) {
+        products_.push_back(product);
     }
 
-    // Example: Booking Room 1 for "Jane Smith"
-    if (service.bookRoom(1, "Jane Smith")) {
-        std::cout << "Room booked successfully!" << std::endl;
+    std::vector<Product> searchProductsByName(const std::string& query) const {
+        std::vector<Product> searchResults;
+        for (const auto& product : products_) {
+            if (containsSubstringIgnoreCase(product.getName(), query)) {
+                searchResults.push_back(product);
+            }
+        }
+        return searchResults;
+    }
+
+private:
+    bool containsSubstringIgnoreCase(const std::string& str, const std::string& substr) const {
+        auto it = std::search(str.begin(), str.end(), substr.begin(), substr.end(),
+                              [](char ch1, char ch2) { return std::toupper(ch1) == std::toupper(ch2); });
+        return it != str.end();
+    }
+
+    std::vector<Product> products_;
+};
+
+int main() {
+    ProductSearchService productSearchService;
+
+    // Adding some products to the search service
+    productSearchService.addProduct(Product("iPhone 12 Pro", 999.99));
+    productSearchService.addProduct(Product("Samsung Galaxy S21", 899.99));
+    productSearchService.addProduct(Product("Google Pixel 5", 799.99));
+    productSearchService.addProduct(Product("Sony WH-1000XM4", 349.99));
+    productSearchService.addProduct(Product("Logitech G Pro X", 129.99));
+
+    // Searching products by name
+    std::cout << "Enter the product name to search: ";
+    std::string query;
+    std::getline(std::cin, query);
+
+    std::vector<Product> searchResults = productSearchService.searchProductsByName(query);
+    if (searchResults.empty()) {
+        std::cout << "No products found matching the search query." << std::endl;
     } else {
-        std::cout << "Room is not available for booking." << std::endl;
+        std::cout << "Search Results:" << std::endl;
+        for (const auto& product : searchResults) {
+            std::cout << product.getName() << " - $" << product.getPrice() << std::endl;
+        }
     }
 
     return 0;
